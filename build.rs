23ad4dbad11d75b6b3da152a46db556d09099ad5
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-04-11 19:57:00 +0200

    examples/key-inverter.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+use std::process::Command;
+
 fn main() {
-    println!("cargo:rustc-link-search=./c-lib/sim/");
+    // makefile is using a special env variable
+    Command::new("make")
+        .env("PLATFORM", "CWLITEARM")
+        .status()
+        .expect("failed to make!");
 }
